c5d9ab53443332f7252769c77057e5ca885c5054
vidya <vsagarj@gmail.com>
2012-11-30 11:09:58 -0800

    init version

---
+++ fiber_dictionary_search.rb
@@ -0,0 +1,81 @@
+require 'pry'
+require 'fiber'
+
+class FiberDictionarySearch
+  attr_accessor :word_pairs
+
+  def initialize(filename)
+    @word_pairs = compute_word_pairs(filename)
+  end
+
+  #--- private ---
+  private
+  def compute_word_pairs(filename)
+    read_seg_fiber             = create_read_segments_fiber(filename)
+    delete_short_words_fiber   = create_delete_short_words_fiber
+    word_pairs_fiber           = create_word_pairs_fiber
+    
+    word_pairs = []
+
+    while read_seg_fiber.alive?
+      word_list            = read_seg_fiber.resume
+      word_list            = delete_short_words_fiber.resume(word_list)
+
+      swap_pairs = word_pairs_fiber.resume(word_list)
+      swap_pairs.each { |pair| word_pairs << pair }
+    end
+
+    word_pairs
+  end
+
+  #--- fiber: read_segments
+  def create_read_segments_fiber(filename)
+    starts_with = lambda do |words, alpha|
+      words.select { |word| word.start_with? alpha }
+    end
+
+    Fiber.new do
+      all_words = File.readlines(filename).map { |ln| ln.chomp }
+
+      ('a'..'z').each do |letter|
+        puts "letter: #{letter}"
+
+        Fiber.yield starts_with[all_words, letter]
+      end
+    end
+  end
+
+  #--- fiber: delete_short_words
+  def create_delete_short_words_fiber
+    Fiber.new do |word_list|
+      while true
+        long_words  = word_list.reject { |w| w.size < 3 }
+
+        next_word_list  = Fiber.yield(long_words)
+        word_list       = next_word_list
+      end
+    end
+  end
+
+  #-- fiber: list_word_pairs
+  def create_word_pairs_fiber
+    Fiber.new do |word_list|
+      while true
+        word_pairs = []
+
+        word_list.each do |word|
+          rev_word = word[0..-3] + word[-2, 2].reverse
+
+          next if rev_word < word
+
+          next if rev_word.eql? word
+
+          word_pairs << [word, rev_word] if word_list.include? rev_word
+        end
+
+        next_word_list  = Fiber.yield(word_pairs)
+        word_list       = next_word_list
+      end
+    end
+  end
+end
